f689b1343922d029bbf4cc72ba54f05709687263
Eric Hong <erichong@gmail.com>
2020-08-26 17:25:55 -0700

    Updates

---
+++ precache-manifest.074e1f2bf6788acdd42af63686d95334.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a677f8af409389b4ae5c68469040c55e",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "39ffaba2a2c9fde28fd2",
+    "url": "/weather/static/css/main.36ff0524.chunk.css"
+  },
+  {
+    "revision": "6b82fdce51f6f03e44f5",
+    "url": "/weather/static/js/2.b16b1c6a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather/static/js/2.b16b1c6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39ffaba2a2c9fde28fd2",
+    "url": "/weather/static/js/main.1bf0e473.chunk.js"
+  },
+  {
+    "revision": "65a135568c88d392bf25",
+    "url": "/weather/static/js/runtime-main.d8725fa2.js"
+  },
+  {
+    "revision": "0832cf0c35137f7c40948ce728655369",
+    "url": "/weather/static/media/013-wind.0832cf0c.png"
+  },
+  {
+    "revision": "45d3c6aaef71dcf52dca2ac920b372a0",
+    "url": "/weather/static/media/014-drop.45d3c6aa.png"
+  },
+  {
+    "revision": "835c892d481672a2bad50b32cf04acf1",
+    "url": "/weather/static/media/015-cloud.835c892d.png"
+  },
+  {
+    "revision": "e07df86cef2e721115583d61d1fb68a6",
+    "url": "/weather/static/media/Roboto-Bold.e07df86c.ttf"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/weather/static/media/Roboto-Regular.11eabca2.ttf"
+  }
+]);
